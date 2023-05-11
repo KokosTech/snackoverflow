e8740313aa292bdb943495c5370b3444e0aad495
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-05-12 00:56:46 +0300

    Revert "Revert "[INIT] Spring Project""
    This reverts commit 44fd1020dc8f54e7dbfcf572aae4f34823c1da37.

---
+++ src/main/java/tech/kaloyan/snackoverflow/SnackoverflowApplication.java
@@ -0,0 +1,13 @@
+package tech.kaloyan.snackoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackoverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnackoverflowApplication.class, args);
+    }
+
+}
