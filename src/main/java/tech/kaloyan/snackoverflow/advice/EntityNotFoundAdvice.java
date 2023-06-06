41bbbd5fb5352d8b244374d9c40e50d71b4f8d9e
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-06-06 22:38:18 +0300

    [ADD] Not found advice
    Signed-off-by: Kaloyan Doychinov <kaloyan@kaloyan.tech>

--- src/main/java/tech/kaloyan/snackoverflow/advice/EntityNotFoundAdvice.java
+++ src/main/java/tech/kaloyan/snackoverflow/advice/EntityNotFoundAdvice.java
@@ -6,9 +6,11 @@ package tech.kaloyan.snackoverflow.advice;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class EntityNotFoundAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = EntityNotFoundException.class)
     public ResponseEntity<?> handleEntityNotFound(EntityNotFoundException exception) {
