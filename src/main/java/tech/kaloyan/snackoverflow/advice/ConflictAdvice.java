d45f56cac75805195ff7d7feaf97b9352391cbeb
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2024-01-22 17:20:27 +0200

    add: more exceptions and advices

---
+++ src/main/java/tech/kaloyan/snackoverflow/advice/ConflictAdvice.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024. Kaloyan Doychinov
+ */
+
+package tech.kaloyan.snackoverflow.advice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import tech.kaloyan.snackoverflow.exeception.ConflictException;
+
+public class ConflictAdvice extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = ConflictException.class)
+    public ResponseEntity<?> handleConflictException(ConflictException exception) {
+        return ResponseEntity.status(409).body(exception.getMessage());
+    }
+}
